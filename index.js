d2ea26e4ed5f56ac462a388229ea5fb290e87ddc
Zamzam <Zamzamhassan44@gmail.com>
2022-06-23 17:18:44 +0300

    created my HTML file content

---
+++ index.js
@@ -0,0 +1,20 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDeatilsContent = document.querySelector('_meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+
+//event listeners
+searchBtn.addEventListener('click', getMealList);
+
+
+//get meal list that matches with ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch('www.themealdb.com/api/json/v1/1/filter.php?i=egg')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+}
