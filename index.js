1a22b59c942ec6af8fc23d8c1e7b980de02f8fdb
Zamzam <Zamzamhassan44@gmail.com>
2022-06-24 18:46:41 +0300

    changed the overall color of my app from orange to rgba(87, 9, 9, 0.79)

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-
-//event listeners
+// event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
 
 
 // get meal list that matches with the ingredients
@@ -16,7 +17,8 @@ function getMealList(){
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
-        let html = " ";{
+        let html = "";
+        if(data.meals){
             data.meals.forEach(meal => {
                 html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
@@ -29,25 +31,30 @@ function getMealList(){
                         </div>
                     </div>
                 `;
-                
             });
-          
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
         }
-        mealList.innerHTML = html;
-    })
 
+        mealList.innerHTML = html;
+    });
 }
-//get recipe of meal
+
+
+// get recipe of the meal
 function getMealRecipe(e){
-e.prevebtDefault();
-if(e.target.classList.contains('recipe-btn')){
-    let mealItem = e.target.parentElement.parentElement;
-    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}')
-    .then(response => response.json())
-    .then(data => mealRecipeModal(data.meals));
-       
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
     }
 }
+
+// create a modal
 function mealRecipeModal(meal){
     console.log(meal);
     meal = meal[0];
@@ -60,11 +67,7 @@ function mealRecipeModal(meal){
         </div>
         <div class = "recipe-meal-img">
             <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
+        </div>`;
     mealDetailsContent.innerHTML = html;
     mealDetailsContent.parentElement.classList.add('showRecipe');
 }
